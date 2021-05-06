640be2fdd8c7bc270e491d4930065b5e0023f042
maxime-bl <maxime.blanchard19@gmail.com>
2021-05-06 09:46:53 +0200

    Création de SaveFiles et son header

---
+++ header/SaveFiles.h
@@ -0,0 +1,8 @@
+//
+// Created by maxim on 06/05/2021.
+//
+
+#ifndef IFE_PROJECT_SAVEFILES_H
+#define IFE_PROJECT_SAVEFILES_H
+
+#endif //IFE_PROJECT_SAVEFILES_H
---
+++ source/SaveFiles.c
@@ -0,0 +1,5 @@
+//
+// Created by maxim on 06/05/2021.
+//
+
+#include "../header/SaveFiles.h"
