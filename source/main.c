a0a9cd9fe63cee84004042335350a4d196d5beb6
maxime-bl <maxime.blanchard19@gmail.com>
2021-05-06 10:31:30 +0200

    Création de SaveFiles et son header

--- source/main.c
+++ source/main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+void mainMenu(){
+    printf("--- BATTLESHIP ---\n\n");
+    printf("1 - Start a new game\n");
+    printf("2 - Load an existing game\n");
+    printf("3 - Quit\n");
+    //scanf(" %d", &)
+}
 int main( )
 {
-    printf("Hello, World!\n");
-    return 0;
-
-    //coucou Bonjour
+    mainMenu();
 }
