5e62d9d50d7f3f61abcd798c5aa25d77e7672e0f
ManonTruchet <manon.truchet2@gmail.com>
2021-05-27 10:22:13 +0200

    fct

--- source/SaveFiles.c
+++ source/SaveFiles.c
@@ -3,4 +3,39 @@
 //
 
 #include "../header/SaveFiles.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+void savefile() {
+
+    FILE* gamesave;
+    gamesave = fopen(".txt","w");
+
+    if (gamesave != NULL){
+
+
+
+        fclose(gamesave);
+    }
+    else{
+        printf("Error: the file cannot be loaded \n")
+    }
+
+}
+
+void loadfile() {
+
+    FILE* gamesave;
+    gamesave = fopen(".txt","r");
+
+    if (gamesave != NULL){
+
+
+
+        fclose(gamesave);
+    }
+    else{
+        printf("Error: the file cannot be loaded \n")
+    }
+
+}
